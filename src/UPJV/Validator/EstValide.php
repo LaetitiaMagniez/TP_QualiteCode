3d49bb82ae79bebdb8d1a8e092889d9694a91e60
LaetitiaMagniez <lmagniez03@gmail.com>
2022-03-23 17:35:25 +0100

    commentaires classe EstValide LaëtitiaMagniez

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,14 +1,31 @@
 <?php
 
+/**
+ * Implementation de la contrainte vérifiant que le formulaire est valide
+ *
+ * @license GNU Public Licence V3
+ */
+
 namespace UPJV\Validator;
 
+/**
+*
+* classe EstValide
+*/
 class EstValide implements ValidatorInterface
 {
+/**
+* function build
+*/
     public function build(array $param): object
     {
         return $this;
     }
 
+/**
+*
+* function check
+*/
     public function check($input): bool
     {
         return true;
