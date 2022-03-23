b952359e538bccfc4d2110655ab3d5ed69b44703
trannois <harold.trannois@u-picardie.fr>
2022-03-23 16:26:29 +0100

    code clean

--- src/UPJV/Validator/EstValide.php
+++ src/UPJV/Validator/EstValide.php
@@ -1,14 +1,37 @@
 <?php
 
+/**
+ * Implementation de la contrainte vérifiant que le formulaire est valide
+ *
+ * @license GNU Public Licence V3
+ */
+
 namespace UPJV\Validator;
 
+/**
+ * EstValide retourne toujours vrai
+ */
 class EstValide implements ValidatorInterface
 {
+    /**
+     * Ne fait rien
+     *
+     * @param array $param
+     *
+     * @return object
+     */
     public function build(array $param): object
     {
         return $this;
     }
 
+    /**
+     * Renvoie toujours true c'est pour la balise hidden contrainte
+     *
+     * @param mixed $input
+     *
+     * @return object
+     */
     public function check($input): bool
     {
         return true;
