a12323f05277904116d11a9f69af117cc90129fa
LaetitiaMagniez <lmagniez03@gmail.com>
2022-03-28 16:45:42 +0200

    création formulaire formLmagniez

---
+++ public/formLmagniez.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulaire</title>
+</head>
+<body>
+    <form action="src/../EstUneImage.php" method="post">
+        <input type="hidden" name="contrainte" value="Laëtitia_EstUneImage">
+        <<form>
+             <label>Pièce Jointe</label>
+             <input type="image" name="piece_jointe" />
+             <button value="Envoyer"></button>
+         </form>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
